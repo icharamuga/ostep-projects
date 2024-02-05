2942f7664b226cb8d766dcb62ce9aebbb5ee5a89
icharamuga <iancharamuga@csus.edu>
2024-02-05 01:08:36 -0800

    wzip now passes test#1

--- initial-utilities/wzip/wzip.c
+++ initial-utilities/wzip/wzip.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv) {
 	if(argc < 2) {
@@ -17,15 +18,40 @@ int main(int argc, char **argv) {
 	}
 
 	char lineText[1000];
+	char *lastChar;
 
 	while(fgets(lineText, 1000, fileIn) != NULL) {
+		lastChar = (char*)malloc(sizeof(char));
+		int lastCharCount = 1;
 		for(int i = 0; i < strlen(lineText); i++) {
-			if(lineText[i] == '\n') {
-				continue;
+			//if(lineText[i] == '\n') {
+				//printf("\n");
+			//}
+			//if(*lastChar == '\n') {
+				//printf("\n");
+			//}
+			if(lineText[i] == *lastChar) {
+				lastCharCount++;
 			}
-			printf("%c", lineText[i]);
+			else if(i != 0) {
+				int written = fwrite(&lastCharCount, 4, 1, stdout);
+				if(written == 0) {
+					printf("error");
+				}
+				printf("%c", *lastChar);
+				lastCharCount = 1;
+			}
+
+			*lastChar = lineText[i];
 		}
-		printf("\n");
+		if(*lastChar == '\n') {
+			printf("\n");
+		}
+		else {
+			fwrite(&lastCharCount, 4, 1, stdout);
+			printf("%c", *lastChar);
+		}
+		free(lastChar);
 	}
 
 	fclose(fileIn);
